29d7af24941e9fb329516a5e470f3c519c050ed9
Siarhey Kozharnovich <tezamteam@gmail.com>
2026-05-05 14:45:26 +0000

    12

--- src/app/components/WarehouseView.tsx
+++ src/app/components/WarehouseView.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback, useMemo } from "react";
+import React, { useState, useEffect, useCallback, useMemo, useRef } from "react";
 import { projectId, publicAnonKey } from "../../../utils/supabase/info";
 import {
   Search, Plus, RefreshCw, Package, ArrowDownToLine, ArrowUpFromLine,
